a076c2d04f21297406539e20f4f74c452ec4078d
RaulTomaz <31389280+RaulTomaz@users.noreply.github.com>
2024-03-18 16:55:08 -0300

    Update numberDigitProductSumDisplay.java

--- codegymcc/numberDigitProductSumDisplay.java
+++ codegymcc/numberDigitProductSumDisplay.java
@@ -1,3 +1,30 @@
+/* 
+Write a program that reads one integer from the keyboard.
+
+If the number consists of three digits, then you need to display the sum of the digits of the number. If it consists of two — the product of digits, if it's one digit — display the number.
+
+In all other cases, do not output anything.
+
+Input example:
+
+123
+Output example:
+
+6
+Input example:
+
+34
+Output example:
+
+12
+Requirements:
+•	The program must read one integer from the keyboard.
+•	The program should display the sum of the digits of the read number, if it is three-digit.
+•	The program should display the product of the digits of the read number, if it is two-digit.
+•	The program should display the read number if it is single digit.
+•	The program should not display anything if the read number has more than three digits.
+*/
+
 package en.codegym.task.pro.task00.task0009;
 
 import java.util.Scanner;
